aae394af22b6b9cd98ff360f7212f1a2d22330bf
Kay Thayer <kayla@atendesigngroup.com>
2020-01-09 15:04:53 -0500

    Update Pantheon settings file

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -755,3 +755,22 @@ $settings['entity_update_backup'] = TRUE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
+
+/**
+ * Include the Pantheon-specific settings file.
+ *
+ * n.b. The settings.pantheon.php file makes some changes
+ *      that affect all envrionments that this site
+ *      exists in.  Always include this file, even in
+ *      a local development environment, to insure that
+ *      the site settings remain consistent.
+ */
+include __DIR__ . "/settings.pantheon.php";
+/**
+ * If there is a local settings file, then include it
+ */
+$local_settings = __DIR__ . "/settings.local.php";
+if (file_exists($local_settings)) {
+  include $local_settings;
+}
+$settings['install_profile'] = 'intercept_profile';
